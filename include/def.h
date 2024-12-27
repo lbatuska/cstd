2d665f27fb9bf0c54021158826591a25fbaae0ec
Levente Batuska <mr.levente97@gmail.com>
2024-12-27 16:31:23 +0100

    feat: add new ptrdifft and min&max values for defined types

--- include/def.h
+++ include/def.h
@@ -4,15 +4,44 @@
 #include "private/linkage_pre.h"
 #include "wordsize.h"
 
+// For unsigned types: max = all bits set to 1, min = 0
+// For signed types: max = (2^bits - 1), min = -(2^bits - 1) - 1
+
 typedef signed char int8;
+#define MAX_INT8 ((int8)((1 << (sizeof(int8) * 8 - 1)) - 1))
+#define MIN_INT8 ((int8)(-(1 << (sizeof(int8) * 8 - 1))))
 typedef unsigned char uint8;
+#define MAX_UINT8 ((uint8)(~(uint8)0))
+#define MIN_UINT8 (0)
+
 typedef signed short int16;
+#define MAX_INT16 ((int16)((1 << (sizeof(int16) * 8 - 1)) - 1))
+#define MIN_INT16 ((int16)(-(1 << (sizeof(int16) * 8 - 1))))
 typedef unsigned short uint16;
+#define MAX_UINT16 ((uint16)(~(uint16)0))
+#define MIN_UINT16 (0)
+
 typedef signed int int32;
+#define MAX_INT16 ((int16)((1 << (sizeof(int16) * 8 - 1)) - 1))
+#define MIN_INT16 ((int16)(-(1 << (sizeof(int16) * 8 - 1))))
 typedef unsigned int uint32;
+#define MAX_UINT32 ((uint32)(~(uint32)0))
+#define MIN_UINT32 (0)
+
 typedef signed long long int64;
+#define MAX_INT64 ((int64)((1 << (sizeof(int64) * 8 - 1)) - 1))
+#define MIN_INT64 ((int64)(-(1 << (sizeof(int64) * 8 - 1))))
 typedef unsigned long long uint64;
+#define MAX_UINT64 ((uint64)(~(uint64)0))
+#define MIN_UINT64 (0)
+
 typedef unsigned long sizet;
+#define MAX_SIZET ((sizet)(~(sizet)0))
+#define MIN_SIZET (0)
+
+typedef typeof((int *)0 - (int *)0) ptrdifft;
+#define MAX_PTRDIFF ((ptrdifft)((1 << (sizeof(ptrdifft) * 8 - 1)) - 1))
+#define MIN_PTRDIFF ((ptrdifft)(-(1 << (sizeof(ptrdifft) * 8 - 1))))
 
 #include "private/linkage_post.h"
 #endif //_DEF_H_
