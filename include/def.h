2d39ab6872c08a2d95716067586a470b5cbb0411
Levente Batuska <mr.levente97@gmail.com>
2024-12-31 02:20:47 +0100

    fix: change ptrdifft to void*, add uintptrt and intptrt min&max definitions
    Signed-off-by: Levente Batuska <mr.levente97@gmail.com>

--- include/def.h
+++ include/def.h
@@ -23,15 +23,15 @@ typedef unsigned short uint16;
 #define MIN_UINT16 (0)
 
 typedef signed int int32;
-#define MAX_INT16 ((int16)((1 << (sizeof(int16) * 8 - 1)) - 1))
-#define MIN_INT16 ((int16)(-(1 << (sizeof(int16) * 8 - 1))))
+#define MAX_INT32 ((int32)((1 << (sizeof(int32) * 8 - 1)) - 1))
+#define MIN_INT32 ((int32)(-(1 << (sizeof(int32) * 8 - 1))))
 typedef unsigned int uint32;
 #define MAX_UINT32 ((uint32)(~(uint32)0))
 #define MIN_UINT32 (0)
 
 typedef signed long long int64;
-#define MAX_INT64 ((int64)((1 << (sizeof(int64) * 8 - 1)) - 1))
-#define MIN_INT64 ((int64)(-(1 << (sizeof(int64) * 8 - 1))))
+#define MAX_INT64 ((int64)((1LL << (sizeof(int64) * 8 - 1)) - 1))
+#define MIN_INT64 ((int64)(-(1LL << (sizeof(int64) * 8 - 1))))
 typedef unsigned long long uint64;
 #define MAX_UINT64 ((uint64)(~(uint64)0))
 #define MIN_UINT64 (0)
@@ -40,12 +40,17 @@ typedef unsigned long sizet;
 #define MAX_SIZET ((sizet)(~(sizet)0))
 #define MIN_SIZET (0)
 
-typedef typeof((int *)0 - (int *)0) ptrdifft;
+typedef typeof((void *)0 - (void *)0) ptrdifft;
 #define MAX_PTRDIFF ((ptrdifft)((1 << (sizeof(ptrdifft) * 8 - 1)) - 1))
 #define MIN_PTRDIFF ((ptrdifft)(-(1 << (sizeof(ptrdifft) * 8 - 1))))
 
 typedef unsigned long uintptrt;
+#define MAX_UINTPTRT ((uintptrt)(~(uintptrt)0))
+#define MIN_UINTPTRT (0)
+
 typedef long intptrt;
+#define MAX_INTPTRT ((intptrt)((1L << (sizeof(intptrt) * 8 - 1)) - 1))
+#define MIN_INTPTRT ((intptrt)(-(1L << (sizeof(intptrt) * 8 - 1))))
 
 #include "private/linkage_post.h"
 #endif //_DEF_H_
