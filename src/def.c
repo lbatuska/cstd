e2e5c76c7debd9c57b4d2e9c25f354044dbdc2ee
Levente Batuska <mr.levente97@gmail.com>
2025-01-11 23:51:26 +0100

    fix: remove unnecessary diagnostics and name struct to avoid warnings

--- src/def.c
+++ src/def.c
@@ -9,16 +9,7 @@
 #define assert_size_in_bits(type, bits)                                        \
   compile_assert_size(sizeof(type) == bit_to_byte(bits));
 
-#if defined __clang__ && !defined __cplusplus
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-declarations"
-
-#elif defined __GNUC__ && !defined __cplusplus
-#pragma GCC diagnostic push
-#pragma gcc diagnostic ignored "-Wmissing-declarations"
-#endif
-
-static const struct __attribute__((unused)) {
+struct __attribute__((unused)) _ {
   assert_size_in_bits(int8, 8);
   assert_size_in_bits(uint8, 8);
   assert_size_in_bits(int16, 16);
@@ -29,10 +20,3 @@ static const struct __attribute__((unused)) {
   assert_size_in_bits(uint64, 64);
   assert_size_in_bits(sizet, WORDSIZE);
 };
-
-#if defined __clang__ && !defined __cplusplus
-#pragma clang diagnostic pop
-
-#elif defined __GNUC__ && !defined __cplusplus
-#pragma GCC diagnostic pop
-#endif
